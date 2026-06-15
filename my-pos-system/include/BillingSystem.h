ea46cbd79450e1924dfb1e7056020008df29d34f
Youssef658177 <josephsalex5@gmail.com>
2026-06-15 19:42:58 +0300

    Add BillingSystem class and CartItem struct

--- my-pos-system/include/BillingSystem.h
+++ my-pos-system/include/BillingSystem.h
@@ -1,1 +1,21 @@
 
+#ifndef BILLINGSYSTEM_H
+#define BILLINGSYSTEM_H
+
+#include "DatabaseManager.h"
+#include <vector>
+
+struct CartItem {
+    Product prod;
+    int qty;
+    double subtotal;
+};
+
+class BillingSystem {
+private:
+    std::vector<CartItem> cart;
+public:
+    void executeInvoiceSession(DatabaseManager& db);
+};
+
+#endif
