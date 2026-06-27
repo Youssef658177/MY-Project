1042e1052fbb16aa898ad8a17b75a32c824fbbc7
Youssef658177 <josephsalex5@gmail.com>
2026-06-28 00:08:55 +0300

    Add Spring Boot application with caching enabled

--- treatme-system/backend/src/main/java/com/treatme/TreatMeApplication.java
+++ treatme-system/backend/src/main/java/com/treatme/TreatMeApplication.java
@@ -1,1 +1,14 @@
 
+package com.treatme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class TreatMeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TreatMeApplication.class, args);
+    }
+}
